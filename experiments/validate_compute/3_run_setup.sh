a912d8e1491c2396a6d6c1bcc897fdcdfc0fa71a
tibi <tiberiu.iancu@gmail.com>
2026-04-22 17:12:09 +0200

    fix(scripts): cd before source in 3_run_setup.sh

--- experiments/validate_compute/3_run_setup.sh
+++ experiments/validate_compute/3_run_setup.sh
@@ -7,8 +7,8 @@
 #SBATCH --error=jobs/setup_%j.err
 set -euo pipefail
 module load 2025 CUDA/12.8.0 cuDNN/9.10.1.4-CUDA-12.8.0 NCCL/2.26.6-GCCcore-14.2.0-CUDA-12.8.0
-source ../../.venv/bin/activate
 cd "$(dirname "$(realpath "$0")")"
+source ../../.venv/bin/activate
 uv pip install pip datasets transformers sentencepiece wget pybind11 torch
 uv pip install --no-build-isolation transformer_engine[pytorch]
 uv pip install -e megatron-lm
