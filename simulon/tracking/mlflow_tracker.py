08b1445844cf752a68360cd3a863347363b39768
tibi <tiberiu.iancu@gmail.com>
2026-06-05 15:50:11 +0200

    debug(tracking): add info logs for experiment name, id, run name

--- simulon/tracking/mlflow_tracker.py
+++ simulon/tracking/mlflow_tracker.py
@@ -25,13 +25,19 @@ class MLflowTracker(ExperimentTracker):
         try:
             experiment_name = os.environ.get("MLFLOW_EXPERIMENT_NAME", "Default")
             run_name = os.environ.get("MLFLOW_RUN_NAME", "simulon")
+            logger.info("MLflow: experiment_name=%r  run_name=%r", experiment_name, run_name)
+
             client = mlflow.tracking.MlflowClient()
             exp = client.get_experiment_by_name(experiment_name)
             exp_id = client.create_experiment(experiment_name) if exp is None else exp.experiment_id
+            logger.info("MLflow: resolved experiment_id=%s", exp_id)
+
             mlflow.start_run(experiment_id=exp_id, run_name=run_name)
             run = mlflow.active_run()
             if run is None:
                 logger.warning("MLflow run was not started (active_run() returned None).")
+            else:
+                logger.info("MLflow: run started id=%s", run.info.run_id)
         except Exception as exc:
             logger.warning("Failed to start MLflow run: %s", exc)
 
