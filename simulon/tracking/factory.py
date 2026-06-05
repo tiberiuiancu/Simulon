68146a8f4fe3bc0e18b83db48a874774815ea36d
tibi <tiberiu.iancu@gmail.com>
2026-06-05 16:19:26 +0200

    switch(tracking): replace MLflow with W&B, update all .tracking.env files

--- simulon/tracking/factory.py
+++ simulon/tracking/factory.py
@@ -6,22 +6,10 @@ from simulon.tracking.base import ExperimentTracker
 
 
 def get_trackers() -> list[ExperimentTracker]:
-    """Return zero or more active trackers based on environment variables.
-
-    MLflow is activated when any of MLFLOW_TRACKING_URI or MLFLOW_EXPERIMENT_NAME is set.
-    W&B is activated when any of WANDB_PROJECT or WANDB_API_KEY is set.
-    Multiple trackers may be active simultaneously.
-    """
+    """Return active W&B tracker based on environment variables."""
     trackers: list[ExperimentTracker] = []
 
-    if any(
-        os.environ.get(k) is not None for k in ("MLFLOW_TRACKING_URI", "MLFLOW_EXPERIMENT_NAME")
-    ):
-        from simulon.tracking.mlflow_tracker import MLflowTracker
-
-        trackers.append(MLflowTracker())
-
-    if any(os.environ.get(k) is not None for k in ("WANDB_PROJECT", "WANDB_API_KEY")):
+    if os.environ.get("WANDB_API_KEY") is not None:
         from simulon.tracking.wandb_tracker import WandbTracker
 
         trackers.append(WandbTracker())
